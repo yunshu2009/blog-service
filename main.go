0b3f73384f488012b8602dd5e82de21ba20a667f
alfred luo <yunshu_luo@163.com>
2020-08-05 23:59:25 +0800

    初始配置读取

--- main.go
+++ main.go
@@ -1,20 +1,55 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/yunshu2009/blog-service/global"
 	"github.com/yunshu2009/blog-service/internal/routers"
+	"github.com/yunshu2009/blog-service/pkg/setting"
 )
 
+// 在init在main之前执行
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err:%v", err)
+	}
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeOut *= time.Second
+	global.ServerSetting.WriteTimeOut *= time.Second
+
+	return nil
+}
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr: ":8080",
-		Handler:router,
-		ReadTimeout:10*time.Second,
-		WriteTimeout:10*time.Second,
-		MaxHeaderBytes:1<<20,
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeOut,
+		WriteTimeout:   global.ServerSetting.WriteTimeOut,
+		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
 }
